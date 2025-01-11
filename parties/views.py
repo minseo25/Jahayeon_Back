a71014f51fb206152cfa3f955e763105ef8480fe
Darwin Jung <darwinjung0721@gmail.com>
2025-01-12 00:01:03 +0900

    party - images

--- parties/views.py
+++ parties/views.py
@@ -1,8 +1,12 @@
+import uuid
 from datetime import datetime
 
 from django.conf import settings
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, parser_classes, permission_classes
+from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from supabase import Client, create_client
@@ -57,15 +61,60 @@ def parties_list(request):
     return Response(parties, status=status.HTTP_200_OK)
 
 
-# TODO: Handle image uploads
+@swagger_auto_schema(
+    method="post",
+    consumes=["multipart/form-data"],
+    manual_parameters=[
+        openapi.Parameter(
+            "images",
+            in_=openapi.IN_FORM,
+            type=openapi.TYPE_ARRAY,
+            items=openapi.Items(type=openapi.TYPE_FILE, format=openapi.FORMAT_BINARY),
+            description="Multiple image files (optional)",
+            required=False,
+        ),
+    ],
+)
 @api_view(["POST"])
 @permission_classes([AllowAny])
+@parser_classes([MultiPartParser, FormParser])
 def parties_create(request):
     party = request.data
+    del party["images"]
 
     party["organizer_id"] = "12b2ac5e-98f6-44be-b790-1305293b52bd"
 
     party = supabase.table("parties").insert(party).execute().data[0]
+
+    images = request.FILES.getlist("images")
+
+    for image in images:
+        file_extension = image.name.split(".")[-1].lower()
+
+        # 파일 데이터를 읽어서 bytes로 변환
+        file_content = image.read()
+
+        # event.id를 사용하여 파일명 생성 (원본 확장자 유지)
+        image_id = str(uuid.uuid4())
+        file_path = f"{image_id}.{file_extension}"
+        upload_response = supabase.storage.from_("images").upload(
+            file_path, file_content
+        )
+
+        if not upload_response.path:
+            return Response(
+                {"error": "Failed to upload image"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
+        # 이미지 테이블에 정보 저장 (URL 구성 수정)
+        public_url = (
+            f"{settings.SUPABASE_URL}/storage/v1/object/public/images/{file_path}"
+        )
+        supabase.table("images").insert(
+            {"id": image_id, "party_id": party["id"], "url": public_url}
+        ).execute()
+
     process_party_response(party)
 
     return Response(party, status=status.HTTP_201_CREATED)
